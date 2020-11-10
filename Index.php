7174c53dc9e8d9757a56917710e46471a53365b9
prashant <prashantamoli2621@gmail.com>
2020-11-10 22:09:42 +0530

    First Commit-Raw

---
+++ Index.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body >
+<div id="header">
+       <br>
+       <h1 style=" font-family:Agency FB; font-size: 70px  ; "> Sparks Foundation Internship Project </h1>
+       <br>
+       <h2 style=" font-family:Agency FB; font-size: 55px  ; "> BASIC BANKING SYSTEM </h2>
+        </div>
+        <div id="section">
+            <table>
+                <tr></tr>
+                <tr>        
+               
+                <div class="css-button" >
+                  <p class="css-button-text">View Users</p>
+                  <div class="css-button-inner">
+                  <a href="getdetail.php" >
+                  <div class="reset-skew">
+                  <p class="css-button-inner-text">View Users</p>
+                </div></a>
+                </div>
+                </div>
+                </td>
+                </tr>
+                <tr>        
+               <br> <br> <br> <br> <br>
+               <div class="css-button" >
+                 <p class="css-button-text">Credit Transfer</p>
+                 <div class="css-button-inner">
+                 <a href="selectuser.php" >
+                 <div class="reset-skew">
+                 <p class="css-button-inner-text">Credit Transfer</p>
+               </div></a>
+               </div>
+               </div>
+               </td>
+               </tr>
+            </table>
+    </div>
+</body>
+</html>
---
+++ UserInfo.php
@@ -0,0 +1,62 @@
+<?php 
+session_start();
+$con=mysqli_connect('localhost','root','','company');
+$name=$_POST['name'];
+$q="select * from user where name='$name'";
+$result=mysqli_query($con,$q);
+$row_count=mysqli_num_rows($result);
+$_SESSION['name']=$name;
+//echo $_SESSION['name'];
+
+?>
+<html>
+<head>
+   <title>
+   viewUser
+    </title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+    <body>
+    <div class="view">
+        <h2>User Information</h2>
+       <table class="flat-table flat-table-1">
+          
+           <th>Name</th>
+           <th>Email</th>
+           <th>Credit</th>
+           <tr>
+           <?php  
+     
+     $row=mysqli_fetch_array($result);
+           ?>
+ <td><?php echo  $row["name"]; ?></td>
+  <td><?php echo  $row["email"]; ?></td>
+  <td><?php echo  $row["credit"]; ?></td>
+   
+
+           </tr>
+
+
+        </table>
+
+        </div>
+        <br>
+        <div class="css-button" >
+                 <p class="css-button-text">Transfer To</p>
+                 <div class="css-button-inner">
+                 <a href="viewuser.php" >
+                 <div class="reset-skew">
+                 <p class="css-button-inner-text">Transfer To</p>
+               </div></a>
+               </div>
+               </div>
+
+
+               <br> <br> <br> <br> <br>
+               
+               
+
+
+    </body>
+</html>
